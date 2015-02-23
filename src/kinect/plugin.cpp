92a58a566cb1340d439e5ed0216aa90022b6ad79
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-02-23 09:28:13 +0100

    Basic fitting works

--- src/kinect/plugin.cpp
+++ src/kinect/plugin.cpp
@@ -20,7 +20,7 @@ class SampleRenderResult : public geo::RenderResult
 public:
 
     SampleRenderResult(cv::Mat& z_buffer_)
-                : geo::RenderResult(z_buffer.cols, z_buffer.rows), z_buffer(z_buffer_), in_view(false) {}
+        : geo::RenderResult(z_buffer_.cols, z_buffer_.rows), z_buffer(z_buffer_), in_view(false) {}
 
     void renderPixel(int x, int y, float depth, int i_triangle)
     {
@@ -72,9 +72,6 @@ void KinectPlugin::process(const ed::WorldModel& world, ed::UpdateRequest& req)
     if (!rgbd_image)
         return;
 
-    cv::imshow("depth", rgbd_image->getDepthImage() / 8);
-    cv::waitKey(3);
-
     // - - - - - - - - - - - - - - - - - -
     // Determine absolte kinect pose based on TF
 
@@ -104,17 +101,17 @@ void KinectPlugin::process(const ed::WorldModel& world, ed::UpdateRequest& req)
     // - - - - - - - - - - - - - - - - - -
     // Render world model based on pose calculated above
 
-    rgbd::View view(*rgbd_image, 120);
+    rgbd::View view(*rgbd_image, 160);
 
     const geo::DepthCamera& rasterizer = view.getRasterizer();
 
     cv::Mat model(view.getHeight(), view.getWidth(), CV_32FC1, 0.0);
 
-    std::cout << "-----" << std::endl;
-
+    std::vector<ed::EntityConstPtr> entities;
     for(ed::WorldModel::const_iterator it = world.begin(); it != world.end(); ++it)
     {
         const ed::EntityConstPtr& e = *it;
+
         if (e->shape())
         {
             geo::Pose3D pose = sensor_pose.inverse() * e->pose();
@@ -126,12 +123,109 @@ void KinectPlugin::process(const ed::WorldModel& world, ed::UpdateRequest& req)
             // Render
             rasterizer.render(opt, res);
 
-            std::cout << e->id() << ": " << res.in_view << std::endl;
+            if (res.in_view)
+                entities.push_back(e);
         }
     }
 
-    cv::imshow("model", model / 8);
+    // - - - - - - - - - - - - - - - - - -
+    // Try other poses and determine best scoring pose
+
+    cv::Mat best_model;
+    double min_error = 1e9; // TODO
+    geo::Pose3D best_pose;
+
+    for(double dx = -0.2; dx < 0.2; dx += 0.05)
+    {
+        for(double dy = -0.2; dy < 0.2; dy += 0.05)
+        {
+            for(double da = -0.1; da < 0.1; da += 0.05)
+            {
+                geo::Matrix3 m;
+                m.setRPY(0, 0, da);
+
+                geo::Pose3D test_pose;
+                test_pose.t = sensor_pose.t + geo::Vector3(dx, dy, 0);
+                test_pose.R = m * sensor_pose.R;
+
+                // Render world
+                cv::Mat model(view.getHeight(), view.getWidth(), CV_32FC1, 0.0);
+                for(std::vector<ed::EntityConstPtr>::const_iterator it = entities.begin(); it != entities.end(); ++it)
+                {
+                    const ed::EntityConstPtr& e = *it;
+
+                    geo::Pose3D pose = test_pose.inverse() * e->pose();
+                    geo::RenderOptions opt;
+                    opt.setMesh(e->shape()->getMesh(), pose);
+
+                    SampleRenderResult res(model);
+
+                    // Render
+                    rasterizer.render(opt, res);
+                }
+
+                int n = 0;
+                double total_error = 0;
+                for(int y = 0; y < view.getHeight(); ++y)
+                {
+                    for(int x = 0; x < view.getWidth(); ++x)
+                    {
+                        float dm = model.at<float>(y, x);
+                        float ds = view.getDepth(x, y);
+
+                        if (dm > 0 && ds > 0) // TODO
+                        {
+                            if (std::abs(dm - ds) > 0.05)
+                                total_error += 1;
+                            ++n;
+                        }
+                    }
+                }
+
+                if (n > 0)
+                {
+                    double avg_error = total_error / n;
+                    if (avg_error < min_error)
+                    {
+                        min_error = avg_error;
+                        best_pose = test_pose;
+                        best_model = model;
+                    }
+                }
+
+
+            }
+        }
+    }
+
+
+    // - - - - - - - - - - - - - - - - - -
+    // Calculate and show diff
+
+    cv::Mat diff(view.getHeight(), view.getWidth(), CV_32FC1, 0.0);
+
+    for(int y = 0; y < view.getHeight(); ++y)
+    {
+        for(int x = 0; x < view.getWidth(); ++x)
+        {
+            float dm = best_model.at<float>(y, x);
+            float ds = view.getDepth(x, y);
+
+            if (dm > 0 && ds > 0)
+            {
+                float err = std::abs(dm - ds);
+                if (err > 0.05)
+                    diff.at<float>(y, x) = err;
+            }
+        }
+    }
+
+    cv::imshow("depth", rgbd_image->getDepthImage() / 8);
+    cv::imshow("initial model", model / 8);
+    cv::imshow("best model", best_model / 8);
+    cv::imshow("diff", diff);
     cv::waitKey(3);
+
 }
 
 // ----------------------------------------------------------------------------------------------------
