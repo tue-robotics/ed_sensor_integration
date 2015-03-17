eec87665dc95b6bc4b9455de00a4dd9adc7e883f
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-03-17 16:22:01 +0100

    Kinect2: renders world model with normals

--- src/kinect2/plugin.cpp
+++ src/kinect2/plugin.cpp
@@ -6,6 +6,11 @@
 #include <rgbd/Image.h>
 #include <rgbd/View.h>
 
+#include <ed/world_model.h>
+#include <ed/entity.h>
+
+#include <geolib/Shape.h>
+
 #include <pcl/point_cloud.h>
 #include <pcl/point_types.h>
 
@@ -17,6 +22,35 @@
 #include <opencv2/highgui/highgui.hpp>
 
 #include <tue/profiling/timer.h>
+
+// ----------------------------------------------------------------------------------------------------
+
+class SampleRenderResult : public geo::RenderResult
+{
+
+public:
+
+    SampleRenderResult(cv::Mat& z_buffer_, cv::Mat& normal_map_)
+        : geo::RenderResult(z_buffer_.cols, z_buffer_.rows), z_buffer(z_buffer_), normal_map(normal_map_), i_normal_offset(0),
+          in_view(false) {}
+
+    void renderPixel(int x, int y, float depth, int i_triangle)
+    {
+        float old_depth = z_buffer.at<float>(y, x);
+        if (old_depth == 0 || depth < old_depth)
+        {
+            z_buffer.at<float>(y, x) = depth;
+            normal_map.at<int>(y, x) = i_normal_offset + i_triangle;
+            in_view = true;
+        }
+    }
+
+    cv::Mat& z_buffer;
+    cv::Mat& normal_map;
+    int i_normal_offset;
+    bool in_view;
+
+};
 
 // ----------------------------------------------------------------------------------------------------
 
@@ -171,6 +205,88 @@ void KinectPlugin::process(const ed::PluginInput& data, ed::UpdateRequest& req)
     }
 
     cv::imshow("normals", viz_normals);
+
+    // - - - - - - - - - - - - - - - - - -
+    // Render world model and calculate normals
+
+    tue::Timer t_render;
+    t_render.start();
+
+    cv::Mat model(depth.rows, depth.cols, CV_32FC1, 0.0);
+    cv::Mat normal_map(depth.rows, depth.cols, CV_32SC1, -1);
+    std::vector<geo::Vector3> model_normals;
+
+    SampleRenderResult res(model, normal_map);
+
+    for(ed::WorldModel::const_iterator it = world.begin(); it != world.end(); ++it)
+    {
+        const ed::EntityConstPtr& e = *it;
+
+        if (e->shape())
+        {
+            const geo::Mesh& mesh = e->shape()->getMesh();
+
+            geo::Pose3D pose = sensor_pose.inverse() * e->pose();
+            geo::RenderOptions opt;
+            opt.setMesh(mesh, pose);
+
+            // Render
+            cam_model.render(opt, res);
+
+            if (res.in_view)
+            {
+                const std::vector<geo::TriangleI>& triangles = mesh.getTriangleIs();
+                const std::vector<geo::Vector3>& vertices = mesh.getPoints();
+
+                for(unsigned int i = 0; i < triangles.size(); ++i)
+                {
+                    const geo::TriangleI& t = triangles[i];
+                    const geo::Vector3& p1 = vertices[t.i1_];
+                    const geo::Vector3& p2 = vertices[t.i2_];
+                    const geo::Vector3& p3 = vertices[t.i3_];
+
+                    // Calculate normal
+                    geo::Vector3 n = ((p3 - p1).cross(p2 - p1)).normalized();
+
+                    model_normals.push_back(sensor_pose.R * n);
+                }
+
+                res.i_normal_offset += triangles.size();
+            }
+        }
+    }
+
+    std::cout << "Rendering (with normals) took " << t_render.getElapsedTimeInMilliSec() << " ms." << std::endl;
+
+
+    // Visualize
+    cv::Mat viz_model_normals(depth.rows, depth.cols, CV_8UC3, cv::Scalar(0, 0, 0));
+
+    for(unsigned int i = 0; i < size; ++i)
+    {
+        int i_normal = normal_map.at<int>(i);
+
+//        std::cout << i_normal << " / " << model_normals.size() << std::endl;
+
+
+        if (i_normal >= 0)
+        {
+            const geo::Vector3& n = model_normals[i_normal];
+            int res = 255;
+
+            int r = res * (n.x + 1) / 2;
+            int g = res * (n.y + 1) / 2;
+            int b = res * (n.z + 1) / 2;
+
+            r *= (255 / res);
+            g *= (255 / res);
+            b *= (255 / res);
+
+            viz_model_normals.at<cv::Vec3b>(i) = cv::Vec3b(b, g, r);
+        }
+    }
+
+    cv::imshow("model_normals", viz_model_normals);
     cv::waitKey(3);
 }
 
