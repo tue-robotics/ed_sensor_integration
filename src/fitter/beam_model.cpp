6da4c5b0e0f527cd7958c0ee71dc0ba240b29cbf
Sjoerd van den Dries <s.v.d.dries@tue.nl>
2015-07-10 11:53:51 +0200

    Fixed bug in beam model rendering

--- src/fitter/beam_model.cpp
+++ src/fitter/beam_model.cpp
@@ -27,6 +27,10 @@ void BeamModel::initialize(double w, unsigned int num_beams)
 
 void BeamModel::RenderModel(const std::vector<std::vector<geo::Vec2> >& contours, const geo::Transform2& pose, std::vector<double>& ranges) const
 {
+    double near_plane = 0.01;
+
+    geo::Vec2 p1_temp, p2_temp;
+
     for(std::vector<std::vector<geo::Vec2> >::const_iterator it_contour = contours.begin(); it_contour != contours.end(); ++it_contour)
     {
         const std::vector<geo::Vec2>& model = *it_contour;
@@ -40,26 +44,53 @@ void BeamModel::RenderModel(const std::vector<std::vector<geo::Vec2> >& contours
         for(unsigned int i = 0; i < model.size(); ++i)
         {
             unsigned int j = (i + 1) % model.size();
-            const geo::Vec2& p1 = t_vertices[i];
-            const geo::Vec2& p2 = t_vertices[j];
+            const geo::Vec2* p1 = &t_vertices[i];
+            const geo::Vec2* p2 = &t_vertices[j];
 
-            int i1 = CalculateBeam(p1.x, p1.y) + 1;
-            int i2 = CalculateBeam(p2.x, p2.y);
+            // If p1 is behind the near plane, clip it
+            if (p1->y < near_plane)
+            {
+                // if p2 is also behind the near plane, skip the line
+                if (p2->y < near_plane)
+                    continue;
 
+                double r = (near_plane - p1->y) / (p2->y - p1->y);
+                p1_temp.x = p1->x + r * (p2->x - p1->x);
+                p1_temp.y = near_plane;
+                p1 = &p1_temp;
+            }
+
+            // If p2 is behind the near plane, clip it
+            if (p2->y < near_plane)
+            {
+                double r = (near_plane - p2->y) / (p1->y - p2->y);
+                p2_temp.x = p2->x + r * (p1->x - p2->x);
+                p2_temp.y = near_plane;
+                p2 = &p2_temp;
+            }
+
+            // Calculate the beam numbers corresponding to p1 and p2
+            int i1 = CalculateBeam(p1->x, p1->y) + 1;
+            int i2 = CalculateBeam(p2->x, p2->y);
+
+            // If i2 < i1, we are looking at the back face of the line, so skip it (back face culling)
+            // If i2 < 0 or i1 >= nbeams, the whole line is out of view, so skip it
             if (i2 < i1 || i2 < 0 || i1 >= nbeams)
                 continue;
 
+            // Clip i1 and i2 to be between 0 and nbeams
             i1 = std::max(0, i1);
             i2 = std::min(i2, nbeams - 1);
 
-            geo::Vec2 s = p2 - p1;
+            geo::Vec2 s = *p2 - *p1;
+            double t = p1->x * s.y - p1->y * s.x;
 
             for(int i_beam = i1; i_beam <= i2; ++i_beam)
             {
                 const geo::Vec2& r = rays_[i_beam];
 
                 // calculate depth of intersection between line (p1, p2) and r
-                double d = (p1.x * s.y - p1.y * s.x) / (r.x * s.y - r.y * s.x);
+                double d =  t / (r.x * s.y - r.y * s.x);
 
                 double& depth_old = ranges[i_beam];
                 if (d > 0 && (d < depth_old || depth_old == 0))
