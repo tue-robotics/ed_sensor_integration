c2bf71772b5f032f131d7ccb658c34ba58aed51b
Janno Lunenburg <jannolunenburg@gmail.com>
2020-12-01 20:03:59 +0100

    wip(test_furniture_fit)Cleaning up

--- ed_sensor_integration/test/test_furniture_fit.cpp
+++ ed_sensor_integration/test/test_furniture_fit.cpp
@@ -122,19 +122,6 @@ void setupRasterizer(image_geometry::PinholeCameraModel& cam_model, geo::DepthCa
 void renderImage(const geo::DepthCamera& rasterizer, const geo::Pose3D& cam_pose, const ed::WorldModel& wm, cv::Mat& depth_image)
 {
     cv::Mat image(depth_image.rows, depth_image.cols, CV_8UC3, cv::Scalar(20, 20, 20));
-    /// TESTCODE: DON'T COMMIT
-    std::cout << "renderImage: getting shape and entity" << std::endl;
-    std::cout << "WM revision: " << wm.revision() << ", entity revision: " << wm.getEntity("table")->revision() << std::endl;
-    wm.getEntity(ed::UUID("table"))->shape();
-    std::cout << "renderImage: getting shape and entity, alternative attempt" << std::endl;
-    wm.getEntity("table")->shape();
-    std::cout << "renderImage: done getting shape and entity" << std::endl;
-    const ed::WorldModel* wm_ptr = &wm;
-    ed::EntityConstPtr e_const_ptr = wm.getEntity("table");
-    const ed::Entity* e_ptr = e_const_ptr.get();
-    std::cout << "WM: " << wm_ptr << ", e: " << e_ptr << std::endl;
-    std::cout << "" << std::endl;
-    /// END OF TESTCODE
     bool result = ed::renderWorldModel(wm, ed::ShowVolumes::NoVolumes, rasterizer, cam_pose, depth_image, image);
     std::cout << "\nRender result: " << result << "\n" << std::endl;
 
@@ -150,72 +137,6 @@ void renderImage(const geo::DepthCamera& rasterizer, const geo::Pose3D& cam_pose
         cv::waitKey(0);
         cv::destroyAllWindows();
     }
-
-//    depth_image.setTo(0.0f);
-
-//    // Iterate over all entities
-//    std::cout << "Iterate over all entities" << std::endl;
-
-//    for(ed::WorldModel::const_iterator it = wm.begin(); it != wm.end(); ++it)
-//    {
-//        std::cout << "\nNext entity... " << std::endl;
-//        const ed::EntityConstPtr& e = *it;
-//        std::cout << "Entity ID: " << e->id() << std::endl;
-        
-////        std::cout << "Getting shaperevision" << std::endl;
-////        int shaperevision = e->shapeRevision();
-////        std::cout << "ShapeRevision: " << shaperevision << std::endl;
-////        if (shaperevision == 0)
-////        {
-////            std::cout << "Entity " << e->id() << " does not have a shape, skipping..." << std::endl;
-////            continue;
-////        }
-
-////        std::cout << "Getting shape" << std::endl;
-////        geo::ShapeConstPtr shape_ptr = e->shape();
-////        std::cout << "\tType: " << shape_ptr->TYPE << std::endl;
-////        std::cout << "\tShapeRevision: " << e->shapeRevision() << std::endl;
-////        std::cout << "\tEmpty: " << shape_ptr->empty() << std::endl;
-////        if (shape_ptr)
-////            std::cout << "Entity " << e->id() << " has a shape of type " << shape_ptr->TYPE << " with shaperevision " << e->shapeRevision() << " and empty: " << shape_ptr->empty() << std::endl;
-////        else
-////        {
-////            std::cout << "Entity " << e->id() << " does not have a shape" << std::endl;
-////            continue;
-////        }
-
-//        // Render ...
-////        std::cout << "Rendering entity " << e->id() << std::endl;
-////        geo::Transform t(-1.0, 0.0, 0.0, 0.0, 0.0, 0.0);
-////        rasterizer.rasterize(*shape_ptr, t, depth_image);
-//        std::cout << "Entity done" << std::endl;
-
-//    }
-//    std::cout << "Iteration done" << std::endl;
-
-
-////    const std::map<std::string, Object*>& objects = world.getObjects();
-////    for(std::map<std::string, Object*>::const_iterator it_obj = objects.begin(); it_obj != objects.end(); ++it_obj) {
-////        Object* obj = it_obj->second;
-
-////        geo::ShapePtr shape = obj->getShape();
-////        if (shape) {
-////            geo::Transform t = tf_map_to_kinect.inverse() * obj->getAbsolutePose();
-////            rasterizer_.rasterize(*shape, t, depth_image);
-////        }
-
-////        std::vector<Object*> children;
-////        obj->getChildrenRecursive(children);
-
-////        for(std::vector<Object*>::const_iterator it = children.begin(); it != children.end(); ++it) {
-////            const Object& child = **it;
-////            geo::ShapePtr child_shape = child.getShape();
-////            if (child_shape) {
-////                geo::Transform t = tf_map_to_kinect.inverse() * child.getAbsolutePose();
-////                rasterizer_.rasterize(*child_shape, t, depth_image);
-////            }
-////        }
-////    }
 
 }
 
@@ -239,9 +160,6 @@ void createWorldModel(ed::WorldModel& wm)
 void moveFurnitureObject(const ed::UUID& id, const geo::Pose3D& new_pose, ed::WorldModel& wm)
 {
     ed::UpdateRequest request;
-//    boost::shared_ptr<ed::TransformCache> t1(new ed::TransformCache());
-//    t1->insert(0, geo::Pose3D(x, y, z, roll, pitch, yaw));
-//    request.setRelation("floor", "table", t1);
     request.setPose(id, new_pose);
     wm.update(request);
 
@@ -271,9 +189,6 @@ bool fitSupportingEntity(const rgbd::Image* image, const geo::Pose3D& sensor_pos
 
     std::cout << "Fitting: getting entity" << std::endl;
     ed::EntityConstPtr e = wm.getEntity(entity_id);
-    e->shape();
-//    std::cout << "Fitting: getting entity shape"
-    // ToDo: what do we want to do if we cannot find the entity?
     if (!e)
         throw std::runtime_error("Entity not found in WM");
     std::cout << "Fitting: estimating entity pose" << std::endl;
@@ -301,7 +216,6 @@ void testSinglePose(const geo::DepthCamera& rasterizer,
 {
     // Move the table
     ed::WorldModel wm_copy(wm);
-//	geo::Pose3D new_pose(x, y, 0.0, 0.0, 0.0, yaw);
     moveFurnitureObject("table", new_pose, wm_copy);
 
     /// Check to see if the pose of the table in the original world model has not changed
@@ -324,12 +238,6 @@ void testSinglePose(const geo::DepthCamera& rasterizer,
     std::cout << "Instantiating 'new pose' " << std::endl;
     geo::Pose3D fitted_pose;
     std::cout << "Fitting supporting entity" << std::endl;
-    //    bool fit_result = fitSupportingEntity(&rgbd_image, cam_pose,
-    //                                          wm_copy, ed::UUID("table"), 45.0 / 180.0 * 3.14, // ToDo: nicer max yaw angle
-    //                                          fitter, new_pose);
-    // ToDo: use wm copy
-    //    const std::string& furniture_id = "table";
-    //    ed::UUID ed_furniture_id(furniture_id);
     ed::UUID ed_furniture_id("table");
     bool fit_result = fitSupportingEntity(&rgbd_image, cam_pose,
               wm, ed_furniture_id, 45.0 / 180.0 * M_PI, // ToDo: nicer max yaw angle
@@ -354,11 +262,6 @@ void testSinglePose(const geo::DepthCamera& rasterizer,
 
 TEST(TestSuite, testCase)
 {
-//    for (unsigned int i = 0; i < 10000; i++)
-//    {
-//        ed::Entity e;
-//        ASSERT_FALSE(e.shape());
-//    }
 
     // Setup world model
     std::cout << "Starting testsuite" << std::endl;
@@ -391,10 +294,8 @@ TEST(TestSuite, testCase)
     for (double x = -0.5; x <= 0.5; x += 0.1)
     {
         for (double y = -0.5; y <= 0.5; y += 0.1)
-//          double y = 0.0;
         {
             for (double yaw_deg = -40.0; yaw_deg <= 40.0; yaw_deg += 10.0)
-//            double yaw_deg = 0.0;
             {
                 ++nr_tests;
                 double yaw = yaw_deg * M_PI / 180.0;
