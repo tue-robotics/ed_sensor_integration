28e992753a151a4004b1d817b79539c56003abca
PetervDooren <PetervDooren@outlook.com>
2021-03-23 21:27:08 +0100

    added boilerplate for tests

---
+++ ed_sensor_integration/test/test_fitter_victor.cpp
@@ -0,0 +1,13 @@
+#include <ed/kinect/fitter.h>
+
+int main(int argc, char **argv)
+{
+    Fitter fitter;
+    FitterData data;
+
+    std::cout << "This is where the fitter shall be tested!!!" << std::endl;
+    //fitter.processSensorData(image, sensor_pose, data);
+    //fitter.estimateEntityPose(data, worldmodel, entity_id, expected_pose, fitted_pose)
+
+    return 0;
+}
