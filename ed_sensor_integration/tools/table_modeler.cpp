d2c3d1b911d349856fe7cd8e59b744056368a56e
Thijs <t.m.h.beurskens@student.tue.nl>
2021-11-15 15:01:52 +0100

    Implement registration algorithm

--- ed_sensor_integration/tools/table_modeler.cpp
+++ ed_sensor_integration/tools/table_modeler.cpp
@@ -1,39 +1,122 @@
-#include <pcl/io/pcd_io.h>
 #include <pcl/point_types.h>
+#include <pcl/point_cloud.h>
+#include <pcl/point_representation.h>
+
+#include <pcl/io/pcd_io.h>
+
+#include <pcl/filters/filter.h>
+
+#include <pcl/features/normal_3d.h>
+
+#include <pcl/registration/icp.h>
+#include <pcl/registration/icp_nl.h>
+#include <pcl/registration/transforms.h>
+
+void pairAlign (const pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_src, const pcl::PointCloud<pcl::PointXYZRGB>::Ptr cloud_tgt, pcl::PointCloud<pcl::PointXYZRGB>::Ptr output, Eigen::Matrix4f &final_transform)
+{
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr src (new pcl::PointCloud<pcl::PointXYZRGB>);
+    pcl::PointCloud<pcl::PointXYZRGB>::Ptr tgt (new pcl::PointCloud<pcl::PointXYZRGB>);
+
+    src = cloud_src;
+    tgt = cloud_tgt;
+
+
+    //
+    // Align
+    pcl::IterativeClosestPointNonLinear<pcl::PointXYZRGB, pcl::PointXYZRGB> reg;
+    reg.setTransformationEpsilon (1e-6);
+    // Set the maximum distance between two correspondences (src<->tgt) to 10cm
+    // Note: adjust this based on the size of your datasets
+    reg.setMaxCorrespondenceDistance (0.1);
+    // Set the point representation
+    // reg.setPointRepresentation (pcl::make_shared<const MyPointRepresentation> (point_representation));
+
+    reg.setInputSource (src);
+    reg.setInputTarget (tgt);
+
+
+
+    //
+    // Run the optimization
+    Eigen::Matrix4f targetToSource;
+    reg.setMaximumIterations (60);
+
+    reg.align (*src);
+
+
+        //
+    // Get the transformation from target to source
+    targetToSource = reg.getFinalTransformation().inverse();
+
+    //
+    // Transform target back in source frame
+    pcl::transformPointCloud (*cloud_tgt, *output, targetToSource);
+
+
+
+    //add the source to the transformed target
+    *output += *cloud_src;
+
+    final_transform = targetToSource;
+ }
+
 
 int main(int argc, char **argv) {
 
+    // open pcd files
+
     if (argc < 2)
+    {
+        std::cout << "Usage:\n\n   table_modeler FILENAME1 FILENAME2 ...\n\n";
+        return 1;
+    }
+
+    //pcl::PointCloud<pcl::PointXYZRGB>::Ptr inputs[argc-1]; //(new pcl::PointCloud<pcl::PointXYZRGB>);
+    std::vector <pcl::PointCloud<pcl::PointXYZRGB>::Ptr, Eigen::aligned_allocator <pcl::PointCloud <pcl::PointXYZRGB>::Ptr > > inputs;
+
+    std::vector<int> indices;
+
+    std::cout << "starting to open" << std::endl;
+
+    for (int i = 1; i < argc; ++i)
+    {
+        std::string name = std::string(argv[i]);
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr m(new pcl::PointCloud<pcl::PointXYZRGB>);
+        std::cout << "attempting to open " << name << std::endl;
+        if (pcl::io::loadPCDFile<pcl::PointXYZRGB> (name, *m) == -1) //* load the file
         {
-            std::cout << "Usage:\n\n   table_modeler FILENAME1 FILENAME2 ...\n\n";
-            return 1;
+            //PCL_ERROR ("Couldn't read file" + name + "\n");
+            return -1;
         }
+        std::cout << "opened " << name << std::endl;
+        pcl::removeNaNFromPointCloud(*m, *m, indices);
+        std::cout << "removed NAN from " << name << std::endl;
+        inputs.push_back(m);
+    }
 
-        pcl::PointCloud<pcl::PointXYZRGB> inputs [argc-1];
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr result (new pcl::PointCloud<pcl::PointXYZRGB>), source, target;
+    Eigen::Matrix4f GlobalTransform = Eigen::Matrix4f::Identity (), pairTransform;
 
-        for (int i = 1; i < argc; ++i)
-        {
-            std::string name = std::string(argv[i]);
+    for (int i = 1; i < argc-1; ++i)
+    {
+        // read json metadata
 
-            // open pcd file
-            if (pcl::io::loadPCDFile<pcl::PointXYZRGB> (name, inputs[i-1]) == -1) //* load the file
-            {
-                //PCL_ERROR ("Couldn't read file" + name + "\n");
-            }
-            else
-            {
+        // align to world model coordinates
 
-            // read json metadata
+        // align to previous file
 
+        source = inputs[i-1];
+        target = inputs[i];
 
-            // align to world model coordinates
+        pcl::PointCloud<pcl::PointXYZRGB>::Ptr temp (new pcl::PointCloud<pcl::PointXYZRGB>);
+        pairAlign (source, target, temp, pairTransform);
 
+        pcl::transformPointCloud (*temp, *result, GlobalTransform);
 
-            // align to previous file
+        GlobalTransform *= pairTransform;
 
-            }
-        }
-
+        pcl::io::savePCDFileASCII ("combined.pcd", *result);
+    }
 
     return 0;
 }
