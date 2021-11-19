cb3fccfa79df9999f5d58813ebfba72f629054b4
Peter van Dooren <pepevandooren@hotmail.com>
2021-11-19 15:31:28 +0100

    Separate segmentation visualization

--- ed_sensor_integration/tools/segmenter.cpp
+++ ed_sensor_integration/tools/segmenter.cpp
@@ -27,7 +27,56 @@
 
 #include "ed_sensor_integration/tools/snapshot.h"
 
+/**
+ * @brief visualizeSegmentation paint segmentation result over an image
+ * @param[in] snapshot snapshot containing the image that was segmented
+ * @param[in] res segmentation result on the image
+ * @param[out] canvas canvas to paint to.
+ */
+void visualizeSegmentation(const Snapshot& snapshot, const UpdateResult& res, cv::Mat canvas)
+{
+    cv::Vec3b fill_colour(0, 0, 255); // red
+    cv::Scalar outline_colour_1(255, 255, 255); // white
+    cv::Scalar outline_colour_2(0, 0, 0); // black
 
+    int depth_width = snapshot.image->getDepthImage().cols;
+    double f = (double)canvas.cols / depth_width;
+
+    for(unsigned int i = 0; i < res.entity_updates.size(); ++i)
+    {
+        const EntityUpdate& e_update = res.entity_updates[i];
+        if (e_update.pixel_indices.empty())
+            continue;
+
+        unsigned i_pxl = e_update.pixel_indices[0];
+        cv::Point bb_min(i_pxl % depth_width, i_pxl / depth_width);
+        cv::Point bb_max(i_pxl % depth_width, i_pxl / depth_width);
+
+        for(std::vector<unsigned int>::const_iterator it = e_update.pixel_indices.begin(); it != e_update.pixel_indices.end(); ++it)
+        {
+            int x = *it % depth_width;
+            int y = *it / depth_width;
+
+            bb_min.x = std::min(bb_min.x, x);
+            bb_min.y = std::min(bb_min.y, y);
+            bb_max.x = std::max(bb_max.x, x);
+            bb_max.y = std::max(bb_max.y, y);
+
+            for(double x2 = f * x; x2 < (f * (x + 1)); ++x2)
+                for(double y2 = f * y; y2 < (f * (y + 1)); ++y2)
+                    canvas.at<cv::Vec3b>(y2, x2) = fill_colour;
+        }
+
+        cv::Point d(2, 2);
+        cv::rectangle(canvas, f * bb_min, f * bb_max, cv::Scalar(255, 255, 255), 2);
+        cv::rectangle(canvas, f * bb_min - d, f * bb_max + d, cv::Scalar(0, 0, 0), 2);
+        cv::rectangle(canvas, f * bb_min + d, f * bb_max - d, cv::Scalar(0, 0, 0), 2);
+    }
+}
+
+/**
+ * @brief usage: instruct user how to use the executable
+ */
 void usage()
 {
     std::cout << "Usage: ed_segmenter IMAGE-FILE-OR-DIRECTORY" << std::endl;
@@ -135,40 +184,7 @@ int main(int argc, char **argv)
         std::string error_msg = res.error.str();
         if (error_msg.empty())
         {
-            int depth_width = snapshot.image->getDepthImage().cols;
-            double f = (double)canvas.cols / depth_width;
-
-            for(unsigned int i = 0; i < res.entity_updates.size(); ++i)
-            {
-                const EntityUpdate& e_update = res.entity_updates[i];
-                if (e_update.pixel_indices.empty())
-                    continue;
-
-                unsigned i_pxl = e_update.pixel_indices[0];
-                cv::Point bb_min(i_pxl % depth_width, i_pxl / depth_width);
-                cv::Point bb_max(i_pxl % depth_width, i_pxl / depth_width);
-
-                for(std::vector<unsigned int>::const_iterator it = e_update.pixel_indices.begin(); it != e_update.pixel_indices.end(); ++it)
-                {
-                    int x = *it % depth_width;
-                    int y = *it / depth_width;
-
-                    bb_min.x = std::min(bb_min.x, x);
-                    bb_min.y = std::min(bb_min.y, y);
-                    bb_max.x = std::max(bb_max.x, x);
-                    bb_max.y = std::max(bb_max.y, y);
-
-                    for(double x2 = f * x; x2 < (f * (x + 1)); ++x2)
-                        for(double y2 = f * y; y2 < (f * (y + 1)); ++y2)
-                            canvas.at<cv::Vec3b>(y2, x2) = cv::Vec3b(0, 0, 255);
-                }
-
-                cv::Point d(2, 2);
-                cv::rectangle(canvas, f * bb_min, f * bb_max, cv::Scalar(255, 255, 255), 2);
-                cv::rectangle(canvas, f * bb_min - d, f * bb_max + d, cv::Scalar(0, 0, 0), 2);
-                cv::rectangle(canvas, f * bb_min + d, f * bb_max - d, cv::Scalar(0, 0, 0), 2);
-            }
-
+            visualizeSegmentation(snapshot, res, canvas);
         }
         else
         {
