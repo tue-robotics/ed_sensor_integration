7fcf0c94aa075424728dbea03325d6f13b36bc05
Peter van Dooren <pepevandooren@hotmail.com>
2022-01-06 15:29:09 +0100

    formatting

--- ed_sensor_integration/src/laser/updater.cpp
+++ ed_sensor_integration/src/laser/updater.cpp
@@ -330,7 +330,6 @@ void LaserUpdater::update(const ed::WorldModel& world, const std::vector<double>
     // - - - - - - - - - - - - - - - - - -
     // Render world model as if seen by laser
 
-    std::cout << "updating laserscan" << std::endl;
     std::vector<double> model_ranges(num_beams, 0);
     renderWorld(sensor_pose, world, model_ranges);
 
