7d967feddf63736adc09852e439f39f8bf1e2aa8
Peter van Dooren <pepevandooren@hotmail.com>
2022-01-15 14:20:58 +0100

    added docstrings

--- ed_sensor_integration/include/ed/kinect/image_buffer.h
+++ ed_sensor_integration/include/ed/kinect/image_buffer.h
@@ -20,6 +20,11 @@ namespace tf
 class TransformListener;
 }
 
+/**
+ * @brief The ImageBuffer class, provides a buffer to synchronise rgbd images with sensor positions.
+ * Images are stored until they are removed from the buffer or until a
+ * certain amount of time has passed
+ */
 class ImageBuffer
 {
 
@@ -29,12 +34,31 @@ public:
 
     ~ImageBuffer();
 
+    /**
+     * @brief initialize, configure the buffer
+     * @param topic, ros topic on which the rgbd images are published
+     * @param root_frame, root frame to calculate sensor pose relative to (default: map)
+     * @param worker_thread_frequency, frequency at which the worker thread updates the most recent image (default: 20)
+     */
     void initialize(const std::string& topic, const std::string& root_frame="map", const float worker_thread_frequency=20);
 
-    // Polls to see if there is a new image with transform. If not, returns false
+    /**
+     * @brief nextImage, Returns the most recent combination of image and transform, provided it is different from the previous time the function was called.
+     *
+     * @param[out] image, rgbd image to write the next image to. Iff a next image is found
+     * @param[out] sensor_pose, will be filled with the sensor pose corresponding to the next image. Iff a next image is found
+     * @return whether or not a novel image is available
+     */
     bool nextImage(rgbd::ImageConstPtr& image, geo::Pose3D& sensor_pose);
 
-    // Blocks until a new image with transform is found. Returns false if no image or tf could be found within 'timeout_sec' seconds
+    /**
+     * @brief waitForRecentImage, Blocks until a new image with transform is found. Returns false if no image or tf could be found within 'timeout_sec' seconds
+     *
+     * @param[out] image, rgbd image to write the next image to. Iff a next image is found
+     * @param[out] sensor_pose, will be filled with the sensor pose corresponding to the next image. Iff a next image is found
+     * @param[in] timeout_sec, maximum duration to block.
+     * @return whether or not a next image was available within the timeout duration.
+     */
     bool waitForRecentImage(rgbd::ImageConstPtr& image, geo::Pose3D& sensor_pose, double timeout_sec);
 
 private:
