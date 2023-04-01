3ae3ea280980783eacd4446562ea580c731c7f63
Peter van Dooren <pepevandooren@hotmail.com>
2023-04-01 11:26:41 +0200

    move implementation to new class

--- ed_sensor_integration/include/ed/kinect/place_area_finder.h
+++ ed_sensor_integration/include/ed/kinect/place_area_finder.h
@@ -1,6 +1,15 @@
 #ifndef ED_KINECT_PLACE_AREA_FINDER_H_
 #define ED_KINECT_PLACE_AREA_FINDER_H_
 
+
+#include <rgbd/image.h>
+#include <geolib/datatypes.h>
+
+
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui.hpp>
+#include <opencv2/core/core.hpp>
+
 class PlaceAreaFinder
 {
 
@@ -10,11 +19,12 @@ public:
 
     ~PlaceAreaFinder();
 
-    void find_area();
+    void findArea(const rgbd::ImageConstPtr& image, geo::Pose3D sensor_pose);
+
+    void getCanvas(cv::Mat& image){ canvas.copyTo(image); }
 
 private:
-
-
+    cv::Mat canvas;
 };
 
 #endif
