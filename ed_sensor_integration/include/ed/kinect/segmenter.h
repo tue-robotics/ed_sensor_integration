5da3044b8d1a1ec1321eb792e2e3d66523afd35e
IasonTheodorou <iasonth95@gmail.com>
2025-09-26 11:07:34 +0200

    added doxygen description for the new functions

--- ed_sensor_integration/include/ed/kinect/segmenter.h
+++ ed_sensor_integration/include/ed/kinect/segmenter.h
@@ -39,9 +39,26 @@ public:
 
     void calculatePointsWithin(const rgbd::Image& image, const geo::Shape& shape,
                                const geo::Pose3D& shape_pose, cv::Mat& filtered_depth_image) const;
-
+    /**
+     * @brief Preprocess RGB image for segmentation. This function masks the RGB image with the filtered depth image (non-zero depth values are kept)
+     * It returns a masked RGB image with values only where depth is non-zero
+     *
+     * @param rgb_image
+     * @param filtered_depth_image
+     * @return cv::Mat filtered_depth_image
+     */
     cv::Mat preprocessRGBForSegmentation(const cv::Mat& rgb_image, const cv::Mat& filtered_depth_image) const;
 
+    /**
+     * @brief Cluster the depth image into segments. Applies new algorithm which is the YOLO - SAM - BMM depth segmentation pipeline.
+     *
+     * @param depth_image
+     * @param cam_model
+     * @param sensor_pose
+     * @param clusters
+     * @param rgb_image
+     * @return std::vector<cv::Mat> masks // 3D pointcloud masks of all the segmented objects
+     */
     std::vector<cv::Mat> cluster(const cv::Mat& depth_image, const geo::DepthCamera& cam_model,
                  const geo::Pose3D& sensor_pose, std::vector<EntityUpdate>& clusters, const cv::Mat& rgb_image);
 
