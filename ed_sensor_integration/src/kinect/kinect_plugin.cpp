749931aee9238e120fbfb2c7239d16121909532a
Matthijs van der Burgh <MatthijsBurgh@outlook.com>
2025-12-16 21:44:31 +0100

    Remove commented code

--- ed_sensor_integration/src/kinect/kinect_plugin.cpp
+++ ed_sensor_integration/src/kinect/kinect_plugin.cpp
@@ -53,7 +53,6 @@ void KinectPlugin::initialize(ed::InitData& init)
     if (config.value("topic", topic))
     {
         ROS_INFO_STREAM("[ED KINECT PLUGIN] Initializing kinect client with topic '" << topic << "'.");
-        //topic = "/usb_cam/image_raw";
         image_buffer_.initialize(topic, "map");
     }
 
